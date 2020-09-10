d74012e0c179092befc84e409705b15c8cddb8b5
wilfried <wilfried.odorico@gmail.com>
2020-09-10 15:46:35 +0200

    mise en place de l'architecture des fichiers, installation de mongoose

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
-app.use(( req, res, next) => {
-    console.log('Requête reçue !');
+mongoose.connect('mongodb+srv://facodeur:facodeur1234@cluster0.je0rp.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use(( req, res, next ) => {
-    res.status(201);
-    next();
-});
+app.use(bodyParser.json());
 
-app.use(( req, res, next ) => {
-    res.json({ message: 'votre requete est reçue !'});
-    next();
-});
-
-app.use(( req, res) => {
-    console.log('Réponse envoyée avec succés !');
-});
-
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
+
+
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv').config();
+// let db = process.env.DB_USER; const pwd = process.env.DB_PASS; let dbName = process.env.DB_NAME;
+// mongoose.connect(`mongodb+srv://${db}:${pwd}@cluster-p-six-oc-kum0q.mongodb.net/${dbName}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
