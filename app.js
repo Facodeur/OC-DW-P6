f132a128c2177ec9da4817cc971a615d4032f93b
wilfried <wilfried.odorico@gmail.com>
2020-09-10 21:41:59 +0200

    renommage de fichier et correction des routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
-const stuffRoutes = require('./routes/stuff');
+const SauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 const app = express();
@@ -22,7 +23,9 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/api/stuff', stuffRoutes);
+app.use('/images', express.static(path.join( __dirname, 'images' )));
+
+app.use('/api/sauces', SauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
