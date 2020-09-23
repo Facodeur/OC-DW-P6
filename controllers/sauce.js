023bccbacf66fdfeaeb11c62eaa181c7186163a6
wilfried <wilfried.odorico@gmail.com>
2020-09-23 13:00:25 +0200

    optimisation du controllers modifySauce pour supprimmer l'ancienne image si elle est changé et changement du systeme de nommage d'image dans multer-config donc utilisation de crypto.randomBytes pour chiffrer le nom d'image

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -30,18 +30,18 @@ exports.likeSauce = (req, res, next) => {
         }).then(() => {
             if (like === 1) {
                 Sauce.updateOne({ _id: req.params.id }, { $push: { usersLiked: user }, $inc: { likes: 1 } })
-                  .then(() => res.status(200).json({ message: user + " j'aime " }))
-                  .catch(error => res.status(400).json({ error }));
+                    .then(() => res.status(200).json({ message: user + " j'aime " }))
+                    .catch(error => res.status(400).json({ error }));
             } else if (like === -1) {
                 Sauce.updateOne({ _id: req.params.id }, { $push: { usersDisliked: user }, $inc: { dislikes: 1 } })
-                  .then(() => res.status(200).json({ message: user + " je n'aime pas " }))
-                  .catch(error => res.status(400).json({ error }));
+                    .then(() => res.status(200).json({ message: user + " je n'aime pas " }))
+                    .catch(error => res.status(400).json({ error }));
             }
 
             if (like === 0) {
                 res.status(200).json({ message: user + " je suis neutre " })
             }
-        }).catch(error => res.status(500).json({ error }));
+        }).catch(error => res.status(404).json({ error }));
 };
 
 exports.createSauce = (req, res, next) => {
@@ -60,10 +60,16 @@ exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ? {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : {...req.body };
-    Sauce.updateOne({ _id: req.params.id }, {...sauceObject, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-        .catch(error => res.status(400).json({ error }));
+    } : { ...req.body };
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        }).catch(error => res.status(404).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
