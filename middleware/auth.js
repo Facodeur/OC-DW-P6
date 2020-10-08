70cc50645f93b4d0d2e1289adc03cf259c5364ad
wilfried <wilfried.odorico@gmail.com>
2020-10-08 14:38:40 +0200

    correction bug lors de la modification de l'image d'une sauce, l'api est fonctionnelle

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
         if (req.body.userId && req.body.userId !== userId) {
             throw 'Invalid user ID';
         } else {
-            req.body.userId = userId;
+            req.internalUserId = userId;
             next();
         }
     } catch {
